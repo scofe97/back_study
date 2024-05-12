3a492437dcbea6b29c58e408ebc1ca68edf5a5cf
scofe97 <tscofet@gmail.com>
2024-05-12 19:33:45 +0900

    202405155

--- java-mid1/src/time/ChangeTimeWithMain.java
+++ java-mid1/src/time/ChangeTimeWithMain.java
@@ -28,5 +28,17 @@ public class ChangeTimeWithMain {
         // 이번 달의 마지막 일요일
         LocalDateTime with2 = dt.with(TemporalAdjusters.lastInMonth(DayOfWeek.SUNDAY));
         System.out.println("같은 달의 마지막 일요일 = " + with2);
+
+
+        LocalDate today = LocalDate.now();
+
+        LocalDate firstDay = today.with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate lastDay = today.with(TemporalAdjusters.lastDayOfMonth());
+        LocalDate nextTuesday  = today.with(TemporalAdjusters.next(DayOfWeek.THURSDAY));
+
+        System.out.println("오늘 날짜: " + today);
+        System.out.println("이번 달의 첫째 날: " + firstDay);
+        System.out.println("이번 달의 마지막 날: " + lastDay);
+        System.out.println("다음 화요일: " + nextTuesday);
     }
 }
